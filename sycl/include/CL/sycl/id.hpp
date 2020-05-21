4c0d28cfa649303aef31df5154e4480df93664a8
Nevin Liber <nliber@anl.gov>
2020-05-21 03:27:03 +0000

    Constrained id variadic constructor to have dimensions number of parameters

--- sycl/include/CL/sycl/id.hpp
+++ sycl/include/CL/sycl/id.hpp
@@ -55,11 +55,13 @@ public:
 
   /* The following constructor is only available in the id struct
    * specialization where: dimensions>=2 */
-  template <
-      typename Dim1, typename Dim2, typename... Dims,
-      typename = detail::enable_if_t<detail::conjunction<
-          std::is_convertible<Dim1, size_t>, std::is_convertible<Dim2, size_t>,
-          std::is_convertible<Dims, size_t>...>::value>>
+  template <typename Dim1, typename Dim2, typename... Dims,
+            typename = detail::enable_if_t<
+                dimensions == 2 + sizeof...(Dims) &&
+                             detail::conjunction<
+                                 std::is_convertible<Dim1, size_t>,
+                                 std::is_convertible<Dim2, size_t>,
+                                 std::is_convertible<Dims, size_t>...>::value>>
   id(Dim1 &&dim1, Dim2 &&dim2, Dims &&... dims)
       : base(std::forward<Dim1>(dim1), std::forward<Dim2>(dim2),
              std::forward<Dims>(dims)...) {}
